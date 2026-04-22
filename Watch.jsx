79897f1e4549a5954ada9b65b86dd482a15734f5
veeren655-ship-it <veeren655@gmail.com>
2026-04-22 18:18:16 +0530

    Add files via upload

--- Watch.jsx
+++ Watch.jsx
@@ -8,20 +8,25 @@ const Watch=()=>{
         utime(time)
     }
     setInterval(fun,1000)
-    return(<>
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-black to-gray-800">
-        <div className="bg-gray-900 text-green-400 px-10 py-6 rounded-2xl shadow-2xl border border-green-500">
+  return (<>
+  <div className="flex items-center justify-center h-screen bg-gradient-to-r from-black to-gray-800">
+    <div className="bg-gray-900 text-green-400 px-10 py-6 rounded-2xl border border-green-500">
 
-            <h1 className="text-5xl md:text-6xl font-mono tracking-widest">
-                {ctime}
-            </h1>
+     <h1 className="text-5xl md:text-6xl font-mono tracking-widest">
+         {ctime} 
+           </h1>
 
-<p className="text-center text-gray-400 mt-3 text-sm">
-    Digital Clock
-</p>
+<p className="text-center text-gray-400 mt-3 text-sm"> 
+    Digital Clock 
+    </p>
 
-        </div>
-    </div></>
-    )
+    </div>
+  </div>
+  
+  
+  </>
+    
+  )
 }
+
 export default Watch
